899bd2b9d1e1d64580ad2db7d9d82f2477ef0969
emmaodia <odia.emma@gmail.com>
2021-07-28 05:46:27 +0100

    chore: updated home page text and configured UI Kitten

--- pages/home.js
+++ pages/home.js
@@ -1,27 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {  Layout, Text, Button } from '@ui-kitten/components';
+import { StyleSheet } from 'react-native';
 
 export function Home({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Layout style={styles.container}>
+      <Text style={{fontFamily: 'scp-regular'}} category='h3' >The is the only mobile app in the world where you can view Chain.Link Price Feeds and set push notifications to receive changes in Token Pair Prices!</Text>
       <Button
-      title="Go to Jane's profile"
+      appearance='ghost'
+      style={styles.button}
+      size='giant'
       onPress={() =>
         navigation.navigate('Wallet')
-      }
-    />
+      } 
+    >{props => <Text {...props}>Launch App</Text>}</Button>
       <StatusBar style="auto" />
-    </View>
+    </Layout>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    // fontFamily: 'space-mono'
+    padding: '10%'
   },
+  button: {
+      fontFamily: 'scp-regular',
+      margin: 20,
+      width: '50%'
+  },
+
 });
