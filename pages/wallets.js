1570e83596037ef1bb319c9ef9595c99824ea0f1
emmaodia <odia.emma@gmail.com>
2021-08-18 23:32:33 +0100

    updated the wallets page with card components.

--- pages/wallets.js
+++ pages/wallets.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { Divider } from '@ui-kitten/components';
+import { CardItem } from '../components/card';
 
-export function Wallet() {
+// import ethUSD from '../priceFeed/ethusd';
+
+export function Wallet({navigation}) {
+
+  // useEffect(() => {
+  //   ethUSD();
+  // }, [])
+
   return (
+   
     <View style={styles.container}>
-      <Text>Wallet!</Text>
+      <TouchableOpacity onPress={()=> navigation.navigate('Prices')}>
+      <Text style={{fontFamily: 'scp-bold'}} category='h1' >Price Feed</Text>
+      </TouchableOpacity>
+      <Divider style={{ margin: 5 }}/>
+      <ScrollView style={{width: '100%'}}>
+      <CardItem 
+      // key={ethUSD}
+      // num={ethUSD}
+      />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
@@ -16,6 +40,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    margin: '1%',
+    paddingTop: '15%',
   },
 });
