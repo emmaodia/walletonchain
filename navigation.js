a8a477a897c88f6adba7f203bcdab7ead509df80
emmaodia <odia.emma@gmail.com>
2021-08-24 23:31:41 +0100

    chore: added pricefeed to pages.

--- navigation.js
+++ navigation.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import { Home } from './pages/home'
 import { Wallet } from './pages/wallets'
+import { Prices } from './pages/pricefeed'
 
 const Stack = createStackNavigator();
 
@@ -10,8 +11,9 @@ export function Navigation() {
     return (
       
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
           <Stack.Screen name="Wallet" component={Wallet} options={{headerShown: false}} />
+          <Stack.Screen name="Prices" component={Prices} options={{headerShown: false}} />
         </Stack.Navigator>
      
     );
